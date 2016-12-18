2472c74f1dd0d74cd3168c236e10bad67f668dbb
A.Fairiz <afairiz@gmail.com>
2016-12-18 19:21:02 +0800

    Initial commit

--- Storm Viewer/ViewController.swift
+++ Storm Viewer/ViewController.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  var pictures = [String]()
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+    let fm = FileManager.default
+    let path = Bundle.main.resourcePath!
+    let items = try! fm.contentsOfDirectory(atPath: path)
+    
+    for item in items{
+      if item.hasPrefix("nssl"){
+        // this is the pic to load
+      }
+    }
   }
 
   override func didReceiveMemoryWarning() {
